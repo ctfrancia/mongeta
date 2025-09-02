b8a1972165071469413b40645b458a18580aa4bc
Christian Francia <christian.francia@tui.com>
2025-09-02 17:20:20 +0200

    fix: finding the right package

--- task/task.go
+++ task/task.go
@@ -2,10 +2,16 @@
 package task
 
 import (
+	"context"
+	"io"
+	"log"
+	"os"
 	"time"
 
 	"github.com/docker/go-connections/nat"
 	"github.com/google/uuid"
+	"github.com/moby/moby/api/types/image"
+	"github.com/moby/moby/client"
 )
 
 type State int
@@ -37,4 +43,42 @@ type TaskEvent struct {
 	State     State
 	TimeStamp time.Time
 	Task      Task
+}
+
+type Config struct {
+	Name          string
+	AttachStdin   bool
+	AttachStdout  bool
+	AttachStderr  bool
+	ExposedPorts  nat.PortSet
+	Cmd           []string
+	Image         string
+	CPU           float64
+	Memory        int64
+	Disk          int64
+	Env           []string
+	RestartPolicy string
+}
+
+type Docker struct {
+	Client *client.Client
+	Config Config
+}
+
+type DockerResult struct {
+	Error       error
+	Action      string
+	ContainerID string
+	Result      string
+}
+
+func (d *Docker) Run(task Task) DockerResult {
+	ctx := context.Background()
+	reader, err := d.Client.ImagePull(ctx, task.Image, image.PullOptions{})
+	if err != nil {
+		log.Printf("Error pulling image %s: %v\n", d.Config.Image, err)
+		return DockerResult{Error: err, Action: "Pull", Result: task.Image}
+	}
+	io.Copy(os.Stdout, reader)
+	return DockerResult{}
 }
