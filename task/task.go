dd84b41c70245263415382aa9eddfbd6d564af5f
Christian Francia <christian.francia@tui.com>
2025-09-03 09:16:32 +0200

    feat: write Run() for task

--- task/task.go
+++ task/task.go
@@ -5,13 +5,16 @@ import (
 	"context"
 	"io"
 	"log"
+	"math"
 	"os"
 	"time"
 
 	"github.com/docker/go-connections/nat"
 	"github.com/google/uuid"
+	"github.com/moby/moby/api/types/container"
 	"github.com/moby/moby/api/types/image"
 	"github.com/moby/moby/client"
+	"github.com/moby/moby/stdcopy"
 )
 
 type State int
@@ -57,7 +60,7 @@ type Config struct {
 	Memory        int64
 	Disk          int64
 	Env           []string
-	RestartPolicy string
+	RestartPolicy container.RestartPolicyMode
 }
 
 type Docker struct {
@@ -80,5 +83,50 @@ func (d *Docker) Run(task Task) DockerResult {
 		return DockerResult{Error: err, Action: "Pull", Result: task.Image}
 	}
 	io.Copy(os.Stdout, reader)
-	return DockerResult{}
+
+	rp := container.RestartPolicy{
+		Name: d.Config.RestartPolicy,
+	}
+
+	r := container.Resources{
+		Memory:   d.Config.Memory,
+		NanoCPUs: int64(d.Config.CPU * math.Pow(10, 9)),
+	}
+
+	cc := container.Config{
+		Image:        d.Config.Image,
+		Tty:          false,
+		Env:          d.Config.Env,
+		ExposedPorts: d.Config.ExposedPorts,
+	}
+
+	hc := container.HostConfig{
+		RestartPolicy:   rp,
+		Resources:       r,
+		PublishAllPorts: true,
+	}
+	resp, err := d.Client.ContainerCreate(ctx, &cc, &hc, nil, nil, d.Config.Name)
+	if err != nil {
+		log.Printf("Error creating container %s: %v\n", d.Config.Name, err)
+		return DockerResult{Error: err, Action: "Create", Result: d.Config.Name}
+	}
+
+	err = d.Client.ContainerStart(ctx, resp.ID, container.StartOptions{})
+	if err != nil {
+		log.Printf("Error starting container %s: %v\n", d.Config.Name, err)
+		return DockerResult{Error: err, Action: "Start", Result: d.Config.Name}
+	}
+
+	// d.Config.Runtime.ContainerID = resp.ID
+	out, err := d.Client.ContainerLogs(
+		ctx,
+		resp.ID,
+		container.LogsOptions{ShowStdout: true, ShowStderr: true},
+	)
+	if err != nil {
+		log.Printf("Error getting logs for container %s: %v\n", d.Config.Name, err)
+		return DockerResult{Error: err, Action: "Logs", Result: d.Config.Name}
+	}
+	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
+	return DockerResult{ContainerID: resp.ID, Action: "Start", Result: "Success"}
 }
